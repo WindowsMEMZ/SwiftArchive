0842a8c08c921f1780eb3e97cadae5d06cf0769c
WindowsMEMZ <3245146430@qq.com>
2023-10-02 23:36:43 +0800

    Added DownloadRes_1:UI

---
+++ BAGen/FSEditorView.swift
@@ -0,0 +1,18 @@
+//
+//  FSEditorView.swift
+//  BAGen
+//
+//  Created by WindowsMEMZ on 2023/10/2.
+//
+
+import SwiftUI
+
+struct FSEditorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FSEditorView()
+}
